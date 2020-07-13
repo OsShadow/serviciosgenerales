8ec2f6ffff8532fce28a1d0583750a9828cd1883
OsShadow <67170735+OsShadow@users.noreply.github.com>
2020-07-13 18:39:38 -0500

    Funcionalidad a los reportes de agua agregados

--- app/Http/Controllers/WaterController.php
+++ app/Http/Controllers/WaterController.php
@@ -8,103 +8,114 @@ use App\WaterReports;
 
 class WaterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
+/**
+ * Display a listing of the resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
+public function index(Request $request)
+{
 
+    $wreports = WaterReports::all();
+    return view('reportes.agua.index', ['wreports' => $wreports]);
 
+}
 
+/**
+ * Show the form for creating a new resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
+public function create()
+{
+    return view('reportes.agua.create');
+}
 
-        $wreports = WaterReports::all();
-        return view('reportes.agua.index', ['wreports' => $wreports]);
+/**
+ * Store a newly created resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\Response
+ */
+public function store(Request $request)
+{
+    $water = new WaterReports();
 
+    $date = Carbon::parse($request->date)->format('Y-m-d');
 
-    }
+    $water->date = $date;
+    $water->start_hour = $request->start_hour;
+    $water->final_hour = $request->final_hour;
+    $water->initial_read = $request->initial_read;
+    $water->final_read = $request->final_read;
+    $water->cloration = $request->cloration;
+    $water->consumption = $request->consumption;
+    $water->consumption_total = $request->consumption_t;
+    $water->observations = $request->observations;
+    $water->user_report = auth()->id();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('reportes.agua.create');
-    }
+    $water->save();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $water = new WaterReports();
+    return redirect('reportes/agua');
+}
 
-        $date = Carbon::parse($request->date)->format('Y-m-d');
+/**
+ * Display the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+public function show($id)
+{
+    return view('reportes.agua.show',['wreport'=> WaterReports::findOrFail($id)]);
+}
 
-        $water->date = $date;
-        $water->start_hour = $request->start_hour;
-        $water->final_hour = $request->final_hour;
-        $water->initial_read = $request->initial_read;
-        $water->final_read = $request->final_read;
-        $water->cloration = $request->cloration;
-        $water->consumption = $request->consumption;
-        $water->consumption_total = $request->consumption_t;
-        $water->observations = $request->observations;
-        $water->user_report = auth()->id();
+/**
+ * Show the form for editing the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+public function edit($id)
+{
+    return view('reportes.agua.edit',['wreport'=> WaterReports::findOrFail($id)]);
+}
 
-        $water->save();
+/**
+ * Update the specified resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+public function update(Request $request, $id)
+{
+    $water = WaterReports::findOrFail($id);
 
-        return redirect('reportes/agua');
-    }
+    $water->initial_read = $request->initial_read;
+    $water->final_read = $request->final_read;
+    $water->cloration = $request->cloration;
+    $water->consumption = $request->consumption;
+    $water->consumption_total = $request->consumption_t;
+    $water->observations = $request->observations;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    $water->update();   
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    return redirect('/reportes/agua');
+}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+/**
+ * Remove the specified resource from storage.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+public function destroy($id)
+{
+    $wreport = WaterReports::findOrFail($id);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    $wreport->delete();
+
+    return redirect('/reportes/agua');
+}
 }
--- resources/views/reportes/agua/edit.blade.php
+++ resources/views/reportes/agua/edit.blade.php
@@ -2,6 +2,82 @@
 
 @section('content')
 
-reporte
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Reporte de Agua') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{route('agua.update', $wreport->id) }}">
+                        @method('PATCH')
+                        @csrf
+                        <div class="form-row">
+                            
+
+                            <div class="form-group col-md-4">
+                                <div class="form-group">
+                                    <label for="Lectura_inicial">Lectura inicial</label>
+                                <input type="text" class="form-control" id="initial_read" name="initial_read" placeholder="" value="{{$wreport->initial_read}}">
+                                  </div>
+
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="form-group">
+                                    <label for="Cloracion">Cloración</label>
+                                    <input type="text" class="form-control" id="cloration" name="cloration" placeholder="Cantidad por litro" value="{{$wreport->cloration}}">
+                                  </div>
+
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <div class="form-group">
+                                    <label for="Lectura_f">Lectura Final</label>
+                                    <input type="text" class="form-control" id="final_read" name="final_read" placeholder="" value="{{$wreport->final_read}}">
+                                  </div>
+
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="form-group">
+                                    <label for="Consumo_m" >Consumo Metros cubicos</label>
+                                      <input class="form-control" type="number"  id="consumption" name="consumption" value="{{$wreport->consumption}}">
+                                  </div>
+
+
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="form-group">
+                                    <label for="Consumo_t" >Consumo Total del mes</label>
+                                      <input class="form-control" type="number"  id="consumption_t" name="consumption_t" value="{{$wreport->consumption_total}}">
+                                  </div>
+
+
+                            </div>
+
+
+                        </div>
+
+
+
+                          <div class="form-group">
+                            <label for="Observaciones">Observaciones</label>
+                          <textarea class="form-control" id="observations" name="observations" rows="3">{{$wreport->Observations}}</textarea>
+                        </div>
+
+
+
+                        <div class="form-group col-md-6">
+
+                                <button type="submit" class="btn btn-primary btn-lg">
+                                    {{ __('Editar Reporte') }}
+                                </button>
+                            
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
