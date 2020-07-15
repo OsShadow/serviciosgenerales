d57e04062f6d2b0eea34e67cc1a70c0579d63524
OsShadow <67170735+OsShadow@users.noreply.github.com>
2020-07-15 17:23:17 -0500

    correccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,12 +149,12 @@
 
 
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4 ">
 <!-- Brand Logo -->
 <a href="{{ url('/') }}" class="brand-link">
     <!--<img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8">-->
-    <span class="brand-text font-weight-light">Servicios Generales</span>
+    <span class="brand-text font-weight-light  " >Servicios Generales</span>
 </a>
 
 <!-- Sidebar -->
@@ -172,7 +172,7 @@
                 {{ Auth::user()->name }}
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-                    Cerrar Sesión
+                    <p style="color:gray">Cerrar Sesión</p> 
                 </a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
                     style="display: none;">
