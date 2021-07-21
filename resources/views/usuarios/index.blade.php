943ac25f332140d0f174976f38d345831c122794
Cristopher Gutierrez <cristophores@gmail.com>
2021-07-20 22:54:03 -0500

    Ajustes en tablas

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('search')
     <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
+    <form class=" col-lg-5">
         <div class="input-group input-group-sm">
             <input class="form-control form-control-navbar" name="search" type="search" placeholder="Buscar por nombre o correo"
                 aria-label="Search">
@@ -16,37 +16,51 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        @can('usuarios.create')
-            <a href="usuarios/create"> <button type="button" style="margin-left: 10px;" class="btn btn-success float-right">
-                    Agregar usuario </button> </a>
-        @endcan
-        <a href="roles"> <button type="button" class="btn btn-success float-right"> Listado de Roles </button> </a>
-        <h2> Lista de usuarios </h2>
+    <div class="">
+        
+        <div class="row  border-bottom white-bg dashboard-header" style="margin-left:-3px;">
+            <div class="col-lg-6">
+                <h2> Lista de usuarios </h2>
+            </div>
+            <div class="col-lg-6">
+                @can('usuarios.create')
+                    <a href="usuarios/create"> 
+                        <button type="button" style="margin-left: 10px;" class="btn btn-primary float-right">Agregar usuario </button> </a>
+                @endcan
+                <a href="roles"> <button type="button" class="btn btn-primary float-right"> Listado de Roles </button> </a>
+                
+            </div>
+        </div>
         <h6>
             @if ($search)
-                <div class="alert alert-primary" role="alert">
-                    Resultados de tu busqueda {{ $search }}
+                <div class="alert alert-info" role="alert">
+                    Resultados de tu busqueda de {{ $search }}
                 </div>
+                
             @endif
         </h6>
-        <table class="table table-striped table-bordered" id="myTable">
+        <table class="table table-striped table-bordered" id="TablaUsuarios">
             <thead>
                 <tr class="thead-dark">
-                    <th scope="col">ID</th>
                     <th data-card-title scope="col">Nombre</th>
                     <th scope="col">Email</th>
+                    <th scope="col">Codigo</th>
                     <th data-card-footer scope="col">Opciones</th>
                 </tr>
             </thead>
             <tbody>
+                @if (count($users) == 0)
+                    <div>
+                        <h4>No hay usuarios para listar</h4>
+                    </div>
+                @endif
                 @foreach ($users as $user)
                     <tr>
-                        <td scope="row">{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
+                        <td scope="row">{{ $user->id }}</td>
                         <td>
-                            <form action="{{ route('usuarios.destroy', $user->id) }}" method="POST">
+                            <form action="{{ route('usuarios.destroy', $user->code) }}" method="POST">
                                 @can('usuarios.show')
                                     <a href="{{ route('usuarios.show', $user->id) }}"><button type="button"
                                             class="btn btn-secondary">Ver</button></a>
