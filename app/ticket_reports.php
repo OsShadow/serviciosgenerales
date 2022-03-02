5a778eb516e78ddda5431ddbd210ec99edd69a9a
Juankjos <juanca171097@gmail.com>
2022-03-02 17:54:11 -0600

    INDEX y CREATE completo, falta editar funciones

--- app/ticket_reports.php
+++ app/ticket_reports.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ticket_reports extends Model
 {
-    //
+    public function ticketStatus(){
+        return $this->hasOne('App\TicketStatus', 'id', 'id_status');
+    }
 }
